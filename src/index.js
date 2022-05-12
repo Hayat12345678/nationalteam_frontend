e2245de1af9c6162a90591ed1b441203d2d9ae83
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-05-12 20:39:25 +0200

    frontend

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "App";
-import { useParam } from "react-router-dom";
-import PlayerList from "./components/PlayerList.jst";
-const root = ReactDOM.createRoot(document.getElementById(root));
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PlayerList from "./components/PlayerList";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/nationalteam" element={<PlayerList />} />
-        </Route>
+        <Route path="/" element={<App />} />
+        <Route path="/nationalteam" element={<PlayerList />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
